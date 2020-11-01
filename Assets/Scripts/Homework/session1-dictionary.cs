b8fd93623661d89189abf334eae94895cb623cb1
jasonshanucl <73372356+jasonshanucl@users.noreply.github.com>
2020-11-01 19:39:43 +0000

    Add files via upload

---
+++ Assets/Scripts/Homework/session1-dictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using system.collections.Generic;
+
+public class Example
+{
+    public static void Main()
+    {
+        Dictionary<string, string> openwith =
+           new Dictionary<string, string>;
+        openwith.add("txt", "notepad.exe");
+        openwith.add("bmp", "paint.exe");
+        openwith.add("dib", "paint.exe");
+        openwith.add("txt", "wordpad.exe");
+        try
+        {
+            openwith.add("txt", "winword.exe");
+        }
+        catch (AggregateException);
+        {
+            Console.WriteLine("An element with key = \"txt\" already exists.");
+        }
+    }
+}
+     
